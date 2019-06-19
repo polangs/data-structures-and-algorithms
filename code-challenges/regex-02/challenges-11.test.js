6bdc416f5271a35f860789574b8ae7c82d49793d
paulacruz <cruz.amp12@gmail.com>
2019-06-18 21:34:35 -0700

    answered 2&3

--- code-challenges/regex-02/challenges-11.test.js
+++ code-challenges/regex-02/challenges-11.test.js
@@ -29,8 +29,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
-};
+  return /^\w+(\.\w+)?@\w+\.(net|com|org)$/i.test(email);};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
@@ -54,7 +53,7 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
+  return /^(\(\d{3}\)[\s]?|\d{3}[\s-]?)\d{3}[\s-]?\d{4}$/.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -93,7 +92,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should match a basic email', () => {
     expect(validateEmail('joe@codefellows.com')).toBeTruthy();
   });
